610974de23edf28d7bb94f6b92a1dcff28c030ae
Ivan <mail.igolubev@gmail.com>
2013-02-03 01:03:45 +0400

    init commit
    Генератор формы на Qooxdoo

---
+++ formGenerator/source/class/formgenerator/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("formgenerator.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ formGenerator/source/class/formgenerator/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("formgenerator.theme.Theme",
+{
+  meta :
+  {
+    color : formgenerator.theme.Color,
+    decoration : formgenerator.theme.Decoration,
+    font : formgenerator.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : formgenerator.theme.Appearance
+  }
+});
