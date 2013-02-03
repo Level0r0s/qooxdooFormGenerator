cd3fec67cf595777511cde0fdad6ca923071800f
Ivan <mail.igolubev@gmail.com>
2013-02-03 16:50:19 +0400

    changes
    Первая разметка для генератора формы

--- formGenerator/source/class/formgenerator/Application.js
+++ formGenerator/source/class/formgenerator/Application.js
@@ -21,55 +21,73 @@ qx.Class.define("formgenerator.Application",
 {
   extend : qx.application.Standalone,
 
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
   members :
   {
-    /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
-     * 
-     * @lint ignoreDeprecated(alert)
-     */
-    main : function()
+    main: function()
     {
-      // Call super class
       this.base(arguments);
 
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
-
       /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
+        Создадим объект со свойствами, которые будут определять форму:
+
       */
 
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "formgenerator/test.png");
+      //массив для single selection list
+      var listData = [];
+      for (var i = 0; i < 25; i++) {
+        listData.push("Item No " + i);
+      }
 
-      // Document is the application root
-      var doc = this.getRoot();
+      var formProperties =
+        [
+          {
+            name: "First column",
+            elements:
+            [{
+              type:  "textfield",
+              label: "Name",
+              labelPosition: "top"
+            },/* {
+              type: "radiobuttongroup",
+              label: "Gender",
+              labelPosition: "left",
+              data: ["Male", "Female"]
+            },*/ {
+              type:  "textfield",
+              label: "Last Name",
+              labelPosition: "left"
+            }, {
+              type:  "textfield",
+              label: "Country",
+              labelPosition: "left"
+            }, {
+              type:  "textfield",
+              label: "City",
+              labelPosition: "left"
+            }]
+          },
+          {
+            name: "Second column",
+            elements:
+            [{
+              type:  "textfield",
+              label: "Additional information",
+              labelPosition: "left"
+            }, {
+              type:  "textarea",
+              label: "Bio",
+              labelPosition: "left"
+            }/*, {
+              type: "singleselection",
+              label: "single",
+              labelPosition: "left",
+              data: listData
+            }*/]
+          }
+        ];
 
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
-      });
+      var formGenerator = new formgenerator.FormGenerator(formProperties);
+      this.getRoot().add(formGenerator, {top: 10, left:10});
     }
   }
 });
---
+++ formGenerator/source/class/formgenerator/FormGenerator.js
@@ -0,0 +1,70 @@
+qx.Class.define("formgenerator.FormGenerator",
+{
+  extend : qx.ui.core.Widget,
+  construct : function(options) {
+  	//console.log(options);
+
+  	this.base(arguments);
+
+    //установили менеджер разметки
+  	var layout = new qx.ui.layout.Grid(10, 10);
+  	this._setLayout(layout);
+
+    //добавляем дочерние виджеты
+    for (var i = 0; i < options.length; i++) {
+      //нам нужно установить менеджер раскладки, и иметь публичные методы для управления дочерними виджетами
+      //поэтому будем использовать Composite вместо обычного виджета
+      var child = new qx.ui.container.Composite();
+      child.setLayout(new qx.ui.layout.Grid(15, 15));
+      //child.add(new qx.ui.basic.Label("ggg"), {row: 0,column: 0});
+      this._add(child, {row: 0, column: i});
+
+      console.log(options[i].elements);
+
+      var row = 0;
+
+      //если есть заголовок, создадим его
+      if (options[i].name) {
+        var label = new qx.ui.basic.Label().set({
+          value: "<b>" + options[i].name + "</b>",
+          rich: true
+        });
+        child.add(label, {row: row, column: 0})
+        row++;
+      }
+
+      var element = null;
+      for (var j = 0; j < options[i].elements.length; j++) {
+        var element = this._createElement(options[i].elements[j]);
+        var label   = new qx.ui.basic.Label(options[i].elements[j].label);
+        child.add(label, {row: row, column: 0});
+        child.add(element, {row: row, column: 1});
+        row++;
+      }
+    }
+
+  	var borderColor = 'black';
+    var border = new qx.ui.decoration.Single(3, "solid", borderColor);
+    this.set({decorator: border, padding: 5, minHeight: 100, minWidth: 100});
+  },
+  members: {
+    _createElement: function(options) {
+      var element = null;
+      switch (options.type) {
+        case "textfield":
+          element = this._createTextField();
+          break;
+        case "textarea":
+          element = this._createTextArea();
+          break;
+      }
+      return element;
+    },
+    _createTextField: function() {
+      return new qx.ui.form.TextField();
+    },
+    _createTextArea:  function() {
+      return new qx.ui.form.TextArea();
+    }
+  }
+});
